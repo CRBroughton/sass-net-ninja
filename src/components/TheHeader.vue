14f13a846d5a10cfbab711220dd36ceccfbb571a
CRBroughton <crbroughton@posteo.uk>
2021-08-04 11:06:49 +0100

    added basic html to TheHeader.vue

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,17 @@
 <template>
-  <header></header>
+  <header>
+    <nav id="main-nav">
+      <div class="wrapper">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Lessons</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script lang="ts">
