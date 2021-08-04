1ffa9ee00370a351566e87ac0f1505a9a954c612
CRBroughton <crbroughton@posteo.uk>
2021-08-04 11:13:41 +0100

    imported .vue files into App.vue Added basic styling to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <TheHeader />
+  <Services />
+  <Lessons />
+  <Projects />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import TheHeader from "@/components/TheHeader.vue";
+import Services from "@/components/Services.vue";
+import Lessons from "@/components/Lessons.vue";
+import Projects from "@/components/Projects.vue";
 
 export default defineComponent({
   components: {
     TheHeader,
+    Services,
+    Lessons,
+    Projects,
   },
   setup() {
     return {};
@@ -17,4 +26,47 @@ export default defineComponent({
 </script>
 
 <style scoped>
+body {
+  font-family: arial;
+  background: #f8f9fb;
+}
+
+html,
+body,
+ul,
+ol,
+li,
+form,
+fieldset,
+legend {
+  margin: 0;
+  padding: 0;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin-top: 0;
+}
+
+fieldset,
+img {
+  border: 0;
+}
+
+li {
+  list-style: none;
+}
+
+.wrapper {
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
 </style>
